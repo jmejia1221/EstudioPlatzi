c3f2abbfcbc70b042a5b6205a93360777b39abf4
John Mejia <jhondario.09@gmail.com>
2015-04-25 18:44:00 -0500

    add news citys template

--- tipografias/js/weather.js
+++ tipografias/js/weather.js
@@ -1,5 +1,7 @@
 (function(){
 
+	var API_WORLDTIME_KEY = "d6a4075ceb419113c64885d9086d5";
+	var API_WORLDTIME = "http://api.worldweatheronline.com/free/v2/tz.ashx?format=json&key=" + API_WORLDTIME_KEY + "&q=";
 	var API_WEATHER_KEY = "80114c7878f599621184a687fc500a12";
 	var API_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?" + API_WEATHER_KEY + "&";
 
@@ -7,6 +9,11 @@
 
 	var today = new Date();
 	var timeNow = today.toLocaleTimeString();
+
+	var $body = $("body");
+	var $loader = $(".loader");
+	var nombreNuevaCiudad = $("[data-input='cityAdd']");
+	var buttonAdd = $("[data-button='add']");
 
 	var cityWeather = {};
 
@@ -16,6 +23,14 @@
 	cityWeather.temp_max;
 	cityWeather.temp_min;
 	cityWeather.main;
+
+	$( buttonAdd ).on("click", addNewCity);
+
+	$( nombreNuevaCiudad ).on("keypress", function(event){
+		if(event.which == 13){
+			addNewCity();
+		}
+	});
 
 	if(navigator.geolocation){
 
@@ -51,7 +66,7 @@
 			cityWeather.main = data.weather[0].main;
 
 			// Render
-			renderTemplate();
+			renderTemplate(cityWeather);
 	};
 
 	function activateTemplate(id){
@@ -59,8 +74,16 @@
 		return document.importNode(t.content, true);
 	};
 
-	function renderTemplate(){
+	function renderTemplate(cityWeather, localtime){
+
 		var clone = activateTemplate("#template--city");
+		var timeToShow;
+
+		if(localtime){
+			timeToShow = localtime.split(" ")[1];
+		}else{
+			timeToShow = timeNow;
+		}
 
 		clone.querySelector("[data-time]").innerHTML = timeNow;
 		clone.querySelector("[data-city]").innerHTML = cityWeather.zone;
@@ -69,8 +92,31 @@
 		clone.querySelector("[data-temp='max']").innerHTML = cityWeather.temp_max.toFixed(1);
 		clone.querySelector("[data-temp='min']").innerHTML = cityWeather.temp_min.toFixed(1);
 
-		$(".loader").hide();
-		$("body").append(clone);
+		$( $loader ).hide();
+		$( $body ).append(clone);
 	};
+
+	function addNewCity(event){
+		event.preventDefault(); 
+		$.getJSON(API_WEATHER_URL + "q=" + $( nombreNuevaCiudad ).val(), getWeatherNewCity );
+	};
+
+	function getWeatherNewCity(data){
+
+		$.getJSON(API_WORLDTIME + $( nombreNuevaCiudad ).val(), function(response){
+
+			cityWeather = {};
+			cityWeather.zone = data.name;
+			cityWeather.icon = IMG_WEATHER + data.weather[0].icon + ".png";
+			cityWeather.temp = data.main.temp - 273.15;
+			cityWeather.temp_max = data.main.temp_max - 273.15;
+			cityWeather.temp_min = data.main.temp_min - 273.15;
+			cityWeather.main = data.weather[0].main;
+
+			renderTemplate(cityWeather, response.data.time_zone[0].localtime);
+
+		});
+	};
+
 
 })();
